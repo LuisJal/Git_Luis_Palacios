ce018e6a552b193d39c40fecbcbedd86854bca91
Luisjal <luispalaciosiglesias@gmail.com>
2022-11-07 20:35:28 +0100

    mi primer commit,root commit.

---
+++ src/Git_Luis_Palacios/LP.java
@@ -0,0 +1,14 @@
+package Git_Luis_Palacios;
+
+public class LP {
+	
+	
+	public static void main(String[] args) {
+		String nombre="Luis";
+		String apellido="Palacios";
+		
+		System.out.print("Alumno: _"+nombre+"_"+apellido);
+
+	}
+
+}
